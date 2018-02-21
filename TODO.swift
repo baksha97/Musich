cf9ff369af4d8a18659ae8efb8ec0dbfd39c4ee5
baksha97 <baksha97@gmail.com>
2018-02-21 02:51:36 -0500

    todo commit

---
+++ TODO.swift
@@ -0,0 +1,9 @@
+//
+//  TODO.swift
+//  Musich
+//
+//  Created by Loaner on 2/21/18.
+//  Copyright © 2018 baksha97. All rights reserved.
+//
+
+//- added files needed to create a fluid registration... just need to add the usage to the ui.
